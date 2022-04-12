edfc6bd61cb6cfd1160e81080cd696a02d8c216a
skdrx09 <jude23712@gmail.com>
2022-04-12 12:24:46 -0300

    Changed uvicorn.add parameters
    Changed uvicorn.add parameters
    
    Changed uvicorn.add parameters.

--- main.py
+++ main.py
@@ -82,4 +82,4 @@ async def del_contact(contact_id: UUID):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info")
