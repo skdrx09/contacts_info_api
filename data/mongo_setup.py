33957c977a7c44525ce442784723478ac59f7405
skdrx09 <jude23712@gmail.com>
2022-04-26 23:09:21 -0300

    Added mongoDB stuff (WIP)

--- data/mongo_setup.py
+++ data/mongo_setup.py
@@ -1,5 +1,9 @@
-from mongoengine import register_connection
+from mongoengine import register_connection, connect
 
 
 def global_init():
-    register_connection(alias='core', name='contacts_api')
+    register_connection(alias='core',
+                        name='ClusterUnit01',
+                        host='mongodb+srv://admin:<1346795>@clusterunit01.ngqrj.mongodb.net/ClusterUnit01?retryWrites=true&w=majority',
+                        port='27017')
+    connect(host='mongodb+srv://admin:<1346795>@clusterunit01.ngqrj.mongodb.net/ClusterUnit01?retryWrites=true&w=majority')
