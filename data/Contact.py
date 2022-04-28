9016fd23826df6f08d903df953fe43f31fcb9247
skdrx09 <jude23712@gmail.com>
2022-04-28 01:16:00 -0300

    Organized and completed some Contact data class info.

--- data/Contact.py
+++ data/Contact.py
@@ -1,16 +1,28 @@
-from mongoengine import *
+from mongoengine.fields import StringField, UUIDField, DateTimeField, EnumField
+from mongoengine import Document
 import datetime
+from enum import Enum
 
 
-class ContactDB:                                                      # DataBase model class
+class Gender(str, Enum):                # Accepts only two gender options
+    male = "male"
+    female = "female"
+
+
+class ContactDB(Document):                                            # DataBase model class
     id = UUIDField                                                    # Generates a random id number
     registered_date = DateTimeField(default=datetime.datetime.now)
     f_name = StringField(required=True)
     l_name = StringField(required=True)
-    gender = StringField(required=False)                              # gender is of Gender class type (male/female)
+    gender = EnumField(Gender, required=False)                                        # gender is of Gender class type (male/female)
 
     meta = {                                                          # Mongodb stuff
         'db_alias': 'core',
         'collection': 'contacts'
     }
 
+
+class ContactUpdateRequest(Document):
+    new_f_name = StringField(required=False)
+    new_l_name = StringField(required=False)
+    new_gender = EnumField(Gender, required=False)
